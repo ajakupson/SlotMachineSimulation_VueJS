ce6af55b75f3bacb8ab9f891fadfab20aa9c3bed
Andrei Jakupson <ajakupson.job@gmail.com>
2020-09-19 14:15:29 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        main: 'app/app.js',
+    },
+    output: {
+        path: path.join(__dirname, 'app/build'),
+        filename: '[name].bundle.js'
+    },
+    mode: 'development'
+}
