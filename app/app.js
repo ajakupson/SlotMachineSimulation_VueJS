f9d8b19f8973c24d089fec4dd189355e81b639d6
Andrei Jakupson <ajakupson.job@gmail.com>
2020-09-19 14:29:05 +0300

    .gitignore changes

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+new Vue({
+    el: '#app',
+    data: {},
+    created: function() {
+        console.log('Vue app created');
+    }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 module.exports = {
     entry: {
-        main: 'app/app.js',
+        main: './app/app.js',
     },
     output: {
-        path: path.join(__dirname, 'app/build'),
+        path: path.join(__dirname, './app/build'),
         filename: '[name].bundle.js'
     },
     mode: 'development'
